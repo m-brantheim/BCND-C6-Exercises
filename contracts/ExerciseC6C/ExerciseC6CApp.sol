f228bd46f5932f2544585ac1f2cb1c9192fece40
Magnus Brantheim <m.brantheim@gmail.com>
2021-10-03 18:52:47 +0200

    Added data contract interface to app contract

--- contracts/ExerciseC6C/ExerciseC6CApp.sol
+++ contracts/ExerciseC6C/ExerciseC6CApp.sol
@@ -6,10 +6,10 @@ pragma solidity ^0.4.25;
 
 import "../../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-
 contract ExerciseC6CApp {
     using SafeMath for uint256; // Allow SafeMath functions to be called for all uint256 types (similar to "prototype" in Javascript)
 
+    ExerciseC6C exerciseC6C;
 
     address private contractOwner;              // Account used to deploy contract
 
@@ -55,24 +55,15 @@ contract ExerciseC6CApp {
                                 external
                                 requireContractOwner
     {
-        updateEmployee(
+        exerciseC6C.updateEmployee(
                         id,
                         amount,
                         calculateBonus(amount)
         );
     }
-
-    function updateEmployee
-                                (
-                                    string id,
-                                    uint256 sales,
-                                    uint256 bonus
-
-                                )
-                                internal
-                                requireContractOwner
-    {
-
-    }
 }
 
+contract ExerciseC6C {
+    function updateEmployee(string id,uint256 sales,uint256 bonus)
+    external;
+}
